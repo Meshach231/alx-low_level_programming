ec594b519d07d31130903ce5a8e4e18f1ecb20ec
adedoyin segun meshach <adedoyinsegun77@gmail.com>
2023-02-16 18:55:42 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - entry point 
  * Description: print "\"Programming is like building a multilingual puzzle"
  * Return: 0
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
