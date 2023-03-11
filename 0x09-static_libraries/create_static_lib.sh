652c40e6f8fe704f7ad32f97958061207e66e8be
adedoyin segun meshach <adedoyinsegun77@gmail.com>
2023-03-10 18:13:25 -0800

    create_static_lib.sh

---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+#!/bin/bash
+gcc -wall -wextra -werror -pedantic -c *.c
+ar rc liball.a *.o
+
